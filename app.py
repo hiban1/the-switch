de4cd1cac392c799f1b2142e06ecb3cccd201857
hiban1 <117941166+hiban1@users.noreply.github.com>
2026-02-28 21:10:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,37 +5,36 @@ import streamlit as st
 produits_carnes = [
 # Voici la liste des burgers de Mcdonald's
     
-    {"nom": "Big Mac", "enseigne": "McDonald's", "prix": 5.70, "proteines": 26, "type": "Burger"},
-    {"nom": "Double Cheese", "enseigne": "McDonald's", "prix": 4.10, "proteines": 24, "type": "Burger"},
-    {"nom": "Royal Cheese", "enseigne": "McDonald's", "prix": 5.90, "proteines": 25, "type": "Burger"},
-    {"nom": "Royal Bacon", "enseigne": "McDonald's", "prix": 6.10, "proteines": 26, "type": "Burger"},
-    {"nom": "McChicken", "enseigne": "McDonald's", "prix": 5.40, "proteines": 20, "type": "Burger"},
-    {"nom": "Triple Cheese", "enseigne": "McDonald's", "prix": 6.20, "proteines": 31, "type": "Burger"},
-    {"nom": "Filet-O-Fish", "enseigne": "McDonald's", "prix": 5.20, "proteines": 15, "type": "Burger"},
-    {"nom": "280 Original", "enseigne": "McDonald's", "prix": 7.40, "proteines": 34, "type": "Burger"},
-    {"nom": "9 McNuggets", "enseigne": "McDonald's", "prix": 6.90, "proteines": 24, "type": "Nuggets"},
+    {"nom": "Big Mac", "enseigne": "McDonald's", "prix": 6.53, "proteines": 26, "type": "Burger"},
+    {"nom": "Double Cheese", "enseigne": "McDonald's", "prix": 5.70, "proteines": 24, "type": "Burger"},
+    {"nom": "Royal Cheese", "enseigne": "McDonald's", "prix": 7.30, "proteines": 25, "type": "Burger"},
+    {"nom": "Royal Bacon", "enseigne": "McDonald's", "prix": 7.23, "proteines": 26, "type": "Burger"},
+    {"nom": "McChicken", "enseigne": "McDonald's", "prix": 5.70, "proteines": 20, "type": "Burger"},
+    {"nom": "Filet-O-Fish", "enseigne": "McDonald's", "prix": 5.70, "proteines": 15, "type": "Burger"},
+    {"nom": "280 Original", "enseigne": "McDonald's", "prix": 8.30, "proteines": 34, "type": "Burger"},
+    {"nom": "9 McNuggets", "enseigne": "McDonald's", "prix": 7.50, "proteines": 24, "type": "Nuggets"},
     
 # Voici la liste des burgers de Burger King
     
-    {"nom": "Whopper", "enseigne": "Burger King", "prix": 6.10, "proteines": 27, "type": "Burger"},
-    {"nom": "Double Whopper", "enseigne": "Burger King", "prix": 7.90, "proteines": 44, "type": "Burger"},
-    {"nom": "Steakhouse", "enseigne": "Burger King", "prix": 7.40, "proteines": 31, "type": "Burger"},
-    {"nom": "Big King", "enseigne": "Burger King", "prix": 5.40, "proteines": 19, "type": "Burger"},
-    {"nom": "Double Cheese Bacon XXL", "enseigne": "Burger King", "prix": 8.10, "proteines": 45, "type": "Burger"},
-    {"nom": "Chicken Louisiane Steakhouse", "enseigne": "Burger King", "prix": 7.20, "proteines": 23, "type": "Burger"},
-    {"nom": "Crispy Chicken Cheese", "enseigne": "Burger King", "prix": 5.20, "proteines": 16, "type": "Burger"},
-    {"nom": "10 King Nuggets", "enseigne": "Burger King", "prix": 6.70, "proteines": 21, "type": "Nuggets"},
+    {"nom": "Whopper", "enseigne": "Burger King", "prix": 6.65, "proteines": 27, "type": "Burger"},
+    {"nom": "Double Whoppe Cheese", "enseigne": "Burger King", "prix": 9.06, "proteines": 44, "type": "Burger"},
+    {"nom": "Steakhouse", "enseigne": "Burger King", "prix": 7.26, "proteines": 31, "type": "Burger"},
+    {"nom": "Big King", "enseigne": "Burger King", "prix": 5.76, "proteines": 19, "type": "Burger"},
+    {"nom": "Double Cheese Bacon XXL", "enseigne": "Burger King", "prix": 9.40, "proteines": 45, "type": "Burger"},
+    {"nom": "Chicken Louisiane Steakhouse", "enseigne": "Burger King", "prix": 7.55, "proteines": 23, "type": "Burger"},
+    {"nom": "Crispy Chicken Cheese", "enseigne": "Burger King", "prix": 5.71, "proteines": 16, "type": "Burger"},
+    {"nom": "10 King Nuggets", "enseigne": "Burger King", "prix": 6.46, "proteines": 21, "type": "Nuggets"},
 
 # Voici la liste des burgers de KFC
 
-    {"nom": "Tower cheese and bacon", "enseigne": "KFC", "prix": 7.50, "proteines": 31, "type": "Burger"},
-    {"nom": "Colonel Original Fish", "enseigne": "KFC", "prix": 6.90, "proteines": 26, "type": "Burger"},
-    {"nom": "Colonel Original", "enseigne": "KFC", "prix": 6.20, "proteines": 24, "type": "Burger"},
-    {"nom": "Boxmaster Original", "enseigne": "KFC", "prix": 8.20, "proteines": 33, "type": "Burger"},
-    {"nom": "Bucket Tenders (x10)", "enseigne": "KFC", "prix": 8.90, "proteines": 48, "type": "Nuggets"},
-    {"nom": "Hot Wings (x8)", "enseigne": "KFC", "prix": 9.50, "proteines": 42, "type": "Nuggets"},
-    {"nom": "Colonel Original Bacon", "enseigne": "KFC", "prix": 8.50, "proteines": 40, "type": "Burger"},
-    {"nom": "Kentucky BBQ Burger", "enseigne": "KFC", "prix": 7.90, "proteines": 28, "type": "Burger"}
+    {"nom": "Tower cheese and bacon", "enseigne": "KFC", "prix": 8.85, "proteines": 31, "type": "Burger"},
+    {"nom": "Colonel Original Fish", "enseigne": "KFC", "prix": 7.15, "proteines": 26, "type": "Burger"},
+    {"nom": "Colonel Original", "enseigne": "KFC", "prix": 7.15, "proteines": 24, "type": "Burger"},
+    {"nom": "Boxmaster Original", "enseigne": "KFC", "prix": 7.90, "proteines": 33, "type": "Burger"},
+    {"nom": "Bucket Tenders (x10)", "enseigne": "KFC", "prix": 17.45, "proteines": 48, "type": "Nuggets"},
+    {"nom": "Hot Wings (x8)", "enseigne": "KFC", "prix": 8.76, "proteines": 42, "type": "Nuggets"},
+    {"nom": "Colonel Original Bacon", "enseigne": "KFC", "prix": 7.66, "proteines": 40, "type": "Burger"},
+    {"nom": "Kentucky BBQ Burger", "enseigne": "KFC", "prix": 9.95, "proteines": 28, "type": "Burger"}
 
     
 ]
@@ -44,17 +43,17 @@ produits_carnes = [
 
 # On ajoute quelques alternatives pour le test
 alternatives_vegans = [
-    {"nom": "Veggie Nuggets (x9)", "enseigne": "McDonald's", "prix": 6.90, "proteines": 15, "type": "Nuggets"},
+    {"nom": "Veggie Nuggets (x9)", "enseigne": "McDonald's", "prix": 7,50, "proteines": 15, "type": "Nuggets"},
     {"nom": "McVeggie", "enseigne": "McDonald's", "prix": 5.70, "proteines": 14, "type": "Burger"},
     
-    {"nom": "Veggie Whopper", "enseigne": "Burger King", "prix": 6.10, "proteines": 22, "type": "Burger"},
-    {"nom": "Veggie Steakhouse", "enseigne": "Burger King", "prix": 7.40, "proteines": 25, "type": "Burger"},
-    {"nom": "Veggie Chicken Louisiane", "enseigne": "Burger King", "prix": 7.60, "proteines": 21, "type": "Burger"},
+    {"nom": "Veggie Whopper", "enseigne": "Burger King", "prix": 6.65, "proteines": 22, "type": "Burger"},
+    {"nom": "Veggie Steakhouse", "enseigne": "Burger King", "prix": 7.36, "proteines": 25, "type": "Burger"},
+    {"nom": "Veggie Chicken Louisiane", "enseigne": "Burger King", "prix": 7.55, "proteines": 21, "type": "Burger"},
     
-    {"nom": "Colonel Original Veggie", "enseigne": "KFC", "prix": 6.20, "proteines": 18, "type": "Burger"},
-    {"nom": "Boxmaster Veggie", "enseigne": "KFC", "prix": 8.20, "proteines": 22, "type": "Burger"},
-    {"nom": "Tower Cheese and Bacon Veggie", "enseigne": "KFC", "prix": 7.50, "proteines": 20, "type": "Burger"},
-    {"nom": "Colonel Original Bacon Veggie", "enseigne": "KFC", "prix": 8.90, "proteines": 21, "type": "Burger"}
+    {"nom": "Colonel Original Veggie", "enseigne": "KFC", "prix": 7.23, "proteines": 18, "type": "Burger"},
+    {"nom": "Boxmaster Veggie", "enseigne": "KFC", "prix": 8.23, "proteines": 22, "type": "Burger"},
+    {"nom": "Tower Cheese and Bacon Veggie", "enseigne": "KFC", "prix": 8.85, "proteines": 20, "type": "Burger"},
+    {"nom": "Colonel Original Bacon Veggie", "enseigne": "KFC", "prix": 7.66, "proteines": 21, "type": "Burger"}
 
 
     
